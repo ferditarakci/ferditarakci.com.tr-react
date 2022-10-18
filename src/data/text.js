793c89d5644f8eb06dd1cff9f8125fb9d032a8b0
Ferdi Tarakcı <bilgi@ferditarakci.com>
2022-10-18 21:34:17 +0300

    Current company

--- src/data/text.js
+++ src/data/text.js
@@ -2,7 +2,7 @@ const Text = {
 	name: "Ferdi Tarakcı",
 	title: "Web Developer | Doğasever | Hayvansever",
 	textLine1: "10 yılı aşkındır front-end ve back-end alanında web tabanlı yazılımlar geliştiriyorum.",
-	textLine2: "Şimdilerde Digidea Agency'de Front-End Web Developer pozisyonunda tam zamanlı olarak çalışıyorum.",
+	textLine2: "Şimdilerde TransferChain'de Front-End Web Developer pozisyonunda tam zamanlı olarak çalışıyorum.",
 	textLine3: "Kullandığım başlıca teknolojiler: JavaScript, React.js, Vue.js, Node.js...",
 }
 
